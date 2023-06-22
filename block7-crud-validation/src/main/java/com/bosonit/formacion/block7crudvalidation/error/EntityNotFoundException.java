c04f5b5375ce4c121030c318108ab8f1b62f9f9c
RaquelAlba <raquel.alba@bosonit.com>
2023-06-22 09:40:43 +0200

    Parte 2: Crear excepciones

--- block7-crud-validation/src/main/java/com/bosonit/formacion/block7crudvalidation/error/EntityNotFoundException.java
+++ block7-crud-validation/src/main/java/com/bosonit/formacion/block7crudvalidation/error/EntityNotFoundException.java
@@ -2,7 +2,11 @@ package com.bosonit.formacion.block7crudvalidation.error;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.HttpStatusCodeException;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class EntityNotFoundException extends RuntimeException{
+public class EntityNotFoundException extends HttpStatusCodeException {
+    public EntityNotFoundException (){
+        super(HttpStatus.NOT_FOUND);
+    }
 }
