ccab712f2c9ceee595aacda7bb107e60b4771807
RaquelAlba <raquel.alba@bosonit.com>
2023-07-20 16:04:44 +0200

    13: Testing con JUnit avanzado

--- block7-crud-validation/src/main/java/com/bosonit/formacion/block7crudvalidation/profesor/controller/dto/ProfesorInputDto.java
+++ block7-crud-validation/src/main/java/com/bosonit/formacion/block7crudvalidation/profesor/controller/dto/ProfesorInputDto.java
@@ -3,7 +3,6 @@ package com.bosonit.formacion.block7crudvalidation.profesor.controller.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
