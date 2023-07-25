75887a24003e187358fa7efda1c95788e3530c01
RaquelAlba <raquel.alba@bosonit.com>
2023-07-25 12:41:13 +0200

    15: Mensajería. Kafka

---
+++ block12-kafka/pruebaKafka2/javaapp.sh
@@ -0,0 +1,1 @@
+java -jar /tmp/pruebaKafka2-0.0.1-SNAPSHOT.jar
---
+++ block12-kafka/pruebaKafka2/src/test/java/com/bosonit/formacion/pruebaKafka2/PruebaKafka2ApplicationTests.java
@@ -0,0 +1,9 @@
+package com.bosonit.formacion.pruebaKafka2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaKafka2ApplicationTests {
+
+}
